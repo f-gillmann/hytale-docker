ef2cfe83451ff53f1863849881dd52181227dfdd
Florian Gillmann <mail@florian-gillmann.com>
2026-01-17 17:45:06 +0100

    docs: add site url to docs

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -4,6 +4,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://f-gillmann.github.io',
+    base: '/hytale-docker',
     integrations: [
         starlight({
             title: 'Hytale Docker',
